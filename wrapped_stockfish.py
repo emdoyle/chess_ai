ce9698a12dab48cc3270385e52567a5673c54c62
Evan M Doyle <evanmdoyle@gmail.com>
2017-10-28 12:33:20 +0200

    Using a couple libraries to generate a GIF for the last match

--- wrapped_stockfish.py
+++ wrapped_stockfish.py
@@ -1,7 +1,12 @@
 import chess
 import hashlib
 import math
-from chess import uci, pgn
+import cairosvg
+import time
+import copy
+import moviepy.editor as mpy
+from scipy.misc import imread
+from chess import uci, pgn, svg
 
 ROOT_DIR = "/Users/evanmdoyle/Programming/ChessAI/"
 
@@ -16,17 +21,34 @@ game.headers["White"] = stockfish.name
 game.headers["Black"] = stockfish.name
 board = chess.Board()
 stockfish.position(board)
+BOARDS = [board]
+
+def write_tmp_png(svg, t):
+    cairosvg.svg2png(bytestring=svg, write_to="/tmp/chesspng"+str(t)+".png")
+
+def make_frame(t):
+    return imread("/tmp/chesspng"+str(int(t))+".png")
 
 move_count = 1
+write_tmp_png(chess.svg.board(board=board), move_count-1)
 node = game
-(best_move,ponder_move) = stockfish.go()
+(best_move,ponder_move) = stockfish.go(movetime=500)
 while(best_move is not None):
-	best_move = chess.Move.from_uci(str(best_move))
-	board.push(best_move)
-	node = node.add_variation(best_move)
-	stockfish.position(board)
-	(best_move, ponder_move)=stockfish.go()
-	move_count += 1
+    if board.can_claim_draw():
+        break
+    best_move = chess.Move.from_uci(str(best_move))
+    board.push(best_move)
+    node = node.add_variation(best_move)
+    stockfish.position(board)
+    BOARDS.append(copy.deepcopy(board))
+    write_tmp_png(chess.svg.board(board=board), move_count)
+    (best_move, ponder_move)=stockfish.go(movetime=500)
+    move_count += 1
+
+clip = mpy.VideoClip(make_frame=make_frame).set_duration(move_count)
+clip.write_gif("chess_game.gif",fps=1, fuzz=30, verbose=False)
+
+print("GIF written to 'chess_game.gif'")
 
 print(board.result())
 print("In " + str(math.floor(move_count/2)) + " Moves")
