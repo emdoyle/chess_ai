86efaa1e0abdc335c6fe9d5e1b750e631001b10f
Evan M Doyle <evanmdoyle@gmail.com>
2017-10-29 23:48:32 +0100

    Derived labels as actual played move in game

--- scripts/process_pgns.py
+++ scripts/process_pgns.py
@@ -12,12 +12,25 @@ PKG_DIR = '/Users/evanmdoyle/Programming/ChessAI/'
 PGN_DIR = os.fsencode(PKG_DIR+'StockfishMirrorMatches/')
 OFF_HEATMAP_DIR = PKG_DIR+'DerivedData/off_heatmaps/'
 DEF_HEATMAP_DIR = PKG_DIR+'DerivedData/def_heatmaps/'
+LABEL_DIR = PKG_DIR+'DerivedData/labels/'
 MATCHES_PATH = PKG_DIR+'StockfishMirrorMatches/'
 SQUARES = chess.SQUARES
 
 for pgn_file in os.listdir(PGN_DIR):
 	file_name = os.fsdecode(pgn_file)
 	game = pgn.read_game(open(MATCHES_PATH+file_name, 'r'))
+
+	board = game.board()
+	moves = 0
+	with open(LABEL_DIR+"labels_"+file_name[:-4]+".csv", 'w') as f:
+		for move in game.main_line():
+			moves += 1
+			label = move.uci()
+			f.write(label+'\n')
+			board.push(move)
+
+	print("Wrote Labels for " + str(moves) + " moves.")
+
 	board = game.board()
 	moves = 0
 	with open(DEF_HEATMAP_DIR+"def_heatmap_"+file_name[:-4]+".csv", 'w') as f:
@@ -31,6 +44,7 @@ for pgn_file in os.listdir(PGN_DIR):
 
 	print("Wrote Defensive Heatmap for " + str(moves) + " moves.")
 
+	board = game.board()
 	moves = 0
 	with open(OFF_HEATMAP_DIR+"off_heatmap_"+file_name[:-4]+".csv", 'w') as f:
 		for move in game.main_line():
