567ee87e49a401c4b2c2af29e6aada01ecac685f
Evan M Doyle <evanmdoyle@gmail.com>
2017-10-29 12:52:03 +0100

    Added argparse to customize engine strength and gif output

--- wrapped_stockfish.py
+++ wrapped_stockfish.py
@@ -4,11 +4,28 @@ import math
 import cairosvg
 import time
 import copy
+import sys
+import argparse
 import moviepy.editor as mpy
 from scipy.misc import imread
 from chess import uci, pgn, svg
 
 ROOT_DIR = "/Users/evanmdoyle/Programming/ChessAI/"
+THREADS = 4
+
+# Allow CLI toggle of GIF since it's slow
+parser = argparse.ArgumentParser(description="Create custom Stockfish mirror matches.")
+parser.add_argument("strength_levels", default=[20,20], nargs='*')
+parser.add_argument("movetime", default=250, nargs='?')
+parser.add_argument("--gif", action='store_true', help="Save GIF of last played match.")
+CLI_args = parser.parse_args()
+
+GIF = CLI_args.gif
+
+STRENGTH_1 = max(0, min(int(CLI_args.strength_levels[0]), 20))
+STRENGTH_2 = max(0, min(int(CLI_args.strength_levels[1]), 20))
+
+MOVETIME = CLI_args.movetime
 
 # Easily open a UCI connection to local stockfish
 stockfish = uci.popen_engine(ROOT_DIR+"stockfish")
@@ -22,7 +39,14 @@ game.headers["White"] = stockfish.name
 game.headers["Black"] = stockfish.name
 board = chess.Board()
 stockfish.position(board)
+stockfish.setoption({'Threads': THREADS}, async_callback=(lambda x: print("Using "+str(THREADS)+" threads.")))
 BOARDS = [board]
+
+def update_strength(turn):
+	if turn:
+		stockfish.setoption({'Skill Level': STRENGTH_1})
+	else:
+		stockfish.setoption({'Skill Level': STRENGTH_2})
 
 # To rasterize svg, convert to png and use pillow to read RGB
 def write_tmp_png(svg, t):
@@ -36,7 +60,8 @@ def make_frame(t):
 move_count = 1
 write_tmp_png(chess.svg.board(board=board), move_count-1)
 node = game
-(best_move,ponder_move) = stockfish.go(movetime=500)
+update_strength(board.turn)
+(best_move,ponder_move) = stockfish.go(movetime=MOVETIME)
 while(best_move is not None):
     if board.can_claim_draw():
         break
@@ -45,15 +70,18 @@ while(best_move is not None):
     node = node.add_variation(best_move)
     stockfish.position(board)
     BOARDS.append(copy.deepcopy(board))
-    write_tmp_png(chess.svg.board(board=board), move_count)
-    (best_move, ponder_move)=stockfish.go(movetime=500)
+    if GIF:
+    	write_tmp_png(chess.svg.board(board=board), move_count)
+    update_strength(board.turn)
+    (best_move, ponder_move)=stockfish.go(movetime=MOVETIME)
     move_count += 1
 
 # Use MoviePy to create a GIF from the png frames
-clip = mpy.VideoClip(make_frame=make_frame).set_duration(move_count)
-clip.write_gif("chess_game.gif",fps=1, fuzz=30, verbose=False)
+if GIF:
+	clip = mpy.VideoClip(make_frame=make_frame).set_duration(move_count)
+	clip.write_gif("chess_game.gif",fps=1, fuzz=30, verbose=False)
 
-print("GIF written to 'chess_game.gif'")
+	print("GIF written to 'chess_game.gif'")
 
 print(board.result())
 print("In " + str(math.floor(move_count/2)) + " Moves")
