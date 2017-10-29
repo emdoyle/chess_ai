cc7a54a54a0b569f208d4d73b551af89b5555cab
Evan M Doyle <evanmdoyle@gmail.com>
2017-10-29 13:12:26 +0100

    Created scripts directory, fiddled with relative imports

--- scripts/process_pgns.py
+++ scripts/process_pgns.py
@@ -1,17 +1,22 @@
+# I absolutely hate this
+import sys
+sys.path.append(sys.path[0] + "/..")
+
 import os
 import numpy as np
 import chess
 import pychess_utils as util
 from chess import pgn
 
-PGN_DIR = os.fsencode('/Users/evanmdoyle/Programming/ChessAI/StockfishMirrorMatches/')
-DATA_DIR = 'DerivedData/'
-LOCAL_PATH = 'StockfishMirrorMatches/'
+PKG_DIR = '/Users/evanmdoyle/Programming/ChessAI/'
+PGN_DIR = os.fsencode(PKG_DIR+'StockfishMirrorMatches/')
+DATA_DIR = PKG_DIR+'DerivedData/'
+MATCHES_PATH = PKG_DIR+'StockfishMirrorMatches/'
 SQUARES = chess.SQUARES
 
 for pgn_file in os.listdir(PGN_DIR):
 	file_name = os.fsdecode(pgn_file)
-	game = pgn.read_game(open(LOCAL_PATH+file_name, 'r'))
+	game = pgn.read_game(open(MATCHES_PATH+file_name, 'r'))
 	board = game.board()
 	moves = 0
 	with open(DATA_DIR+"stockfishdata_"+file_name[:-4]+".csv", 'w') as f:
