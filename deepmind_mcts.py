b8d60d8434fc022278112fdff3db8be69efbbb9e
Evan M Doyle <evanmdoyle@gmail.com>
2017-11-02 02:40:42 +0100

    Add color to DOT, start deepmind mcts modifications

--- deepmind_mcts.py
+++ deepmind_mcts.py
@@ -12,45 +12,20 @@ from chess import pgn, uci
 from collections import defaultdict
 
 DRAW = 'draw'
+# This constant should change exploration tendencies
+CPUCT = 1.5
 
-def get_siblings(node):
-	return [x for x in node.parent.children if x != node]
+# NN = network.load()
 
-def total_sims_at_depth(node):
-	sims = node.simulations
-	for sibling in get_siblings(node):
-		sims += sibling.simulations
-	return sims
+class Edge:
 
-
-def selection_formula(node):
-	w = node.wins
-	n = node.simulations
-	N = total_sims_at_depth(node)
-	c = math.sqrt(2)
-	term1 = float(w/n)
-	term2 = c*math.sqrt(math.log(N)/n)
-	return term1 + term2
-
-class Node:
-
-	def __init__(self, color, parent=None, position=None):
-		self.color = color
-		self.move = None
-		self.wins = 0
-		self.simulations = 0
-		self.color = True
-		self.parent = parent
-		self.children = []
-		self.position = position
-
-	@property
-	def position(self):
-		return self.__position
-
-	@position.setter
-	def position(self, position):
-		self.__position = position if type(position) == chess.Board else None
+	def __init__(self, node, move, prob, simulations=0, total_action_value=0, action_value=0):
+		self.node = node
+		self.prob = prob
+		self.total_action_value = total_action_value
+		self.action_value = action_value
+		self.simulations = simulations
+		self.move = move
 
 	@property
 	def move(self):
@@ -61,12 +36,12 @@ class Node:
 		self.__move = move if type(move) == chess.Move else None
 
 	@property
-	def wins(self):
-		return self.__wins
+	def prob(self):
+		return self.__prob
 
-	@wins.setter
-	def wins(self, wins):
-		self.__wins = wins if wins >= 0 else 0
+	@prob.setter
+	def prob(self, prob):
+		self.__prob = prob if prob >= 0 else 0
 
 	@property
 	def simulations(self):
@@ -75,6 +50,37 @@ class Node:
 	@simulations.setter
 	def simulations(self, sims):
 		self.__simulations = sims if sims >=0 else 0
+
+	def get_siblings(self):
+		return [y for x, y in self.node.parent.children if y != self]
+
+	def total_sims_at_depth(self):
+		sims = self.simulations
+		for sibling in self.get_siblings():
+			sims += sibling.simulations
+		return sims
+
+	def get_confidence(self):
+		term1 = CPUCT*self.prob
+		term2 = math.sqrt(self.total_sims_at_depth)/(1 + self.simulations)
+		return term1*term2
+
+
+class Node:
+
+	def __init__(self, color, parent=None, position=None):
+		self.position = position
+		self.color = color
+		self.parent = parent
+		self.children = []
+
+	@property
+	def position(self):
+		return self.__position
+
+	@position.setter
+	def position(self, position):
+		self.__position = position if type(position) == chess.Board else None
 
 	@property
 	def children(self):
@@ -92,18 +98,40 @@ class Node:
 	def parent(self, parent):
 		self.__parent = parent
 
-	def add_child(self, node):
-		self.__children.append(node)
-
 
 
 class MCTS:
+
+	TEMPERATURE = True
 
 	def __init__(self, startpos=chess.Board(), iter_time=100):
 		self.startpos = startpos
 		self.__root = Node(True, position=chess.Board())
 		self.iter_time = iter_time
-		self.__states = []
+
+	def max_action_val_child(root):
+		max_val = 0
+		max_child = None
+		max_edge = None
+		for child, edge in root.children:
+			if edge.action_value + edge.get_confidence() > max_val:
+				max_child = child
+				max_edge = edge
+		return (max_child, max_edge)
+
+	def most_visited_child(root):
+		max_visits = 0
+		max_child = None
+		for child, edge in root.children:
+			if edge.simulations > max_visits:
+				max_child = child
+		return max_child
+
+	def total_child_visits(root):
+		visits = 0
+		for child, edge in root.children:
+			visits += edge.simulations
+		return visits
 
 	@property
 	def startpos(self):
@@ -112,10 +140,6 @@ class MCTS:
 	@startpos.setter
 	def startpos(self, startpos):
 		self.__startpos = startpos if type(startpos) == chess.Board else chess.Board()
-
-	def add_state(self, state):
-		if type(state) == chess.Board:
-			self.__states.append(state)
 
 	@property
 	def iter_time(self):
@@ -129,96 +153,67 @@ class MCTS:
 		begin = datetime.datetime.utcnow()
 		while datetime.datetime.utcnow() - begin < datetime.timedelta(seconds=self.iter_time):
 			leaf = self.select_leaf(self.__root)
-			leaves = self.expand_tree(leaf)
-			winners = []
-			for new_leaf in leaves:
-				winners.append((new_leaf, self.playout(new_leaf)))
-			self.backprop(winners)
-
+			self.expand_tree(leaf)
+			self.backprop(leaf, NN.value(leaf.position))
 
 	def select_leaf(self, root):
-		curr_node = root
-		if not curr_node.children:
+		if not root.children:
 			return root
-
-		node_scores = defaultdict(int)
-		for child in curr_node.children:
-			if not child.children:
-				if not child.position.is_game_over(claim_draw=True):
-					node_scores[child] = selection_formula(child)
-			else:
-				temp = self.select_leaf(child)
-				if not temp.position.is_game_over(claim_draw=True):
-					node_scores[temp] = selection_formula(temp)
-
-		# 0-index first time to get top score, second time to get node from (node, score)
-		return sorted(node_scores.items(), key=operator.itemgetter(1), reverse=True)[0][0]
+		else:
+			return select_leaf(max_action_val_child(root)[0])
 
 	def expand_tree(self, leaf):
 		if leaf.position:
 			new_leaves = []
 			board = leaf.position
+			priors = NN.policy(board)
 			moves = list(board.legal_moves)
 			if len(moves) == 0:
 				print("No moves from position: " + board.fen() + "\n")
 			for selected_move in moves:
 				new_board = copy.deepcopy(board)
+				# Make sure you can index into priors with a UCI move
+				new_edge = Edge(leaf, selected_move, priors[selected_move.uci()])
 				new_board.push(selected_move)
 				new_node = Node(not leaf.color, parent=leaf, position=new_board)
-				new_node.move = selected_move
-				leaf.children.append(new_node)
+				leaf.children.append((leaf, new_edge))
 				new_leaves.append(new_node)
 			return new_leaves
 		else:
-			print("MCTS has invalid start position.")
+			print("MCTS tried to expand with empty position.")
 
-	def decode(self, result):
-		if result == '1-0':
-			print("White Wins!")
-			return True
-		elif result == '0-1':
-			print("Black Wins!")
-			return False
-		else:
-			return DRAW
-
-	def playout(self, leaf):
-		board = copy.deepcopy(leaf.position)
-		while not board.is_game_over(claim_draw=True):
-			moves = list(board.legal_moves)
-			selected_move = choice(moves)
-			board.push(selected_move)
-		return self.decode(board.result(claim_draw=True))
-
-	def backprop(self, winners):
-		for leaf, result in winners:
-			while leaf:
-				if leaf.color == result:
-					leaf.wins += 1
-				elif result == DRAW:
-					leaf.wins += 0.5
-				leaf.simulations += 1
-				leaf = leaf.parent
+	def backprop(self, leaf, value):
+		leaf = leaf.parent
+		while leaf:
+			path_edge = max_action_val_child(leaf)[1]
+			path_edge.simulations += 1
+			path_edge.total_action_value += value
+			path_edge.action_value = path_edge.total_action_value/path_edge.simulations
+			leaf = leaf.parent
 
 	def best_move(self):
+		# TODO totally different algorithm to pick a move
 		node = self.__root
 		child_scores = []
-		for child in node.children:
+		for child, edge in node.children:
+			# TODO fix this to use edges
 			child_scores.append((child.move.uci(), (child.wins/child.simulations)))
 		return sorted(child_scores, key=operator.itemgetter(1), reverse=True)[0][0]
 
 	def recurs_print(self, node, file_handle):
+		# Fully broken, need to fix DOT to label edges and use them to traverse here
 		string_set = "{ "
-		for child in node.children:
+		for child, edge in node.children:
 			string_set += str(id(child)) + " [label=\"sims" + str(child.simulations) + "wins" + str(child.wins) + "color" + str(child.color) + "\"] "
 		string_set += "}"
 		file_handle.write(str(id(node)) + " [label=\"sims" + str(node.simulations) + "wins" + str(node.wins) + "color" + str(node.color) + '\"];')
 		file_handle.write(str(id(node)) + ' -> ' + string_set + ';\n')
-		for child in node.children:
+		for child, edge in node.children:
 			self.recurs_print(child, file_handle)
 
 
 	def print_tree(self):
+		# Fully broken, see recurs_print
 		node = self.__root
 		with open("/Users/evanmdoyle/Programming/ChessAI/MCTS.dot", 'w') as f:
 			f.write("digraph G { \n")
